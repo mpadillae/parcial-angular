45c2a07b16e7c37646ae49af875a8dc74e402f1c
Miguel Fernando Padilla Espino <m.padillae@uniandes.edu.co>
2024-04-25 20:16:24 -0500

    implementing the vehicle service

--- src/app/vehicle/vehicle.service.ts
+++ src/app/vehicle/vehicle.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { Vehicle } from './vehicle';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VehicleService {
 
-constructor() { }
+  private url: string = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getVehicles(): Observable<Vehicle[]> {
+    return this.http.get<Vehicle[]>(this.url);
+  }
 
 }
